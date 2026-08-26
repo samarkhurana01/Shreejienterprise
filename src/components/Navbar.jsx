ab80fd84fd30282e59e3f37cdc9851ae6bf0010d
samarkhurana01 <samarkhurana014@gmail.com>
2026-08-27 01:22:01 +0530

    Refactor Navbar component with state management

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,133 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+
 import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <header className="navbar">
-      <div className="navbar-inner">
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <Link to="/" className="brand">
-          <span className="brand-mark">SJ</span>
-          <span>ShreeJi Enterprises</span>
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <header className="site-header">
+
+      <div className="navbar-container">
+
+        <Link
+          to="/"
+          className="brand"
+          onClick={closeMenu}
+        >
+          <span className="brand-mark">
+            SJ
+          </span>
+
+          <span className="brand-name">
+            ShreeJi Enterprises
+          </span>
         </Link>
 
-        <nav className="navbar-links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About Us</Link>
-          <Link to="/services">Our Services</Link>
-          <Link to="/labour-categories">
+
+        <nav
+          className={`desktop-nav ${
+            menuOpen ? "mobile-open" : ""
+          }`}
+        >
+
+          <NavLink
+            to="/"
+            end
+            onClick={closeMenu}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            onClick={closeMenu}
+          >
+            About Us
+          </NavLink>
+
+          <NavLink
+            to="/services"
+            onClick={closeMenu}
+          >
+            Our Services
+          </NavLink>
+
+          <NavLink
+            to="/labour-categories"
+            onClick={closeMenu}
+          >
             Labour Categories
-          </Link>
-          <Link to="/industries">
+          </NavLink>
+
+          <NavLink
+            to="/industries-served"
+            onClick={closeMenu}
+          >
             Industries Served
-          </Link>
-          <Link to="/contact">
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            onClick={closeMenu}
+          >
             Contact Us
-          </Link>
+          </NavLink>
+
+          <a
+            className="mobile-call-button"
+            href="tel:+917379433305"
+            onClick={closeMenu}
+          >
+            <span className="material-symbols-outlined">
+              call
+            </span>
+
+            Call +91 73794 33305
+          </a>
+
         </nav>
 
+
         <a
-          href="tel:+919876543210"
-          className="navbar-call"
+          href="tel:+917379433305"
+          className="header-call"
         >
-          CALL NOW
+          <span className="material-symbols-outlined">
+            call
+          </span>
+
+          <span>Call Now</span>
         </a>
 
+
+        <button
+          type="button"
+          className={`menu-toggle ${
+            menuOpen ? "active" : ""
+          }`}
+          onClick={() =>
+            setMenuOpen((current) => !current)
+          }
+          aria-label={
+            menuOpen
+              ? "Close navigation"
+              : "Open navigation"
+          }
+          aria-expanded={menuOpen}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+
       </div>
+
     </header>
   );
 }
