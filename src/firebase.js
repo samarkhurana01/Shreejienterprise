08b5b146f56ed757db62c2dca1b65b5f08788ad5
samarkhurana01 <samarkhurana014@gmail.com>
2026-08-27 01:49:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,51 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-  query,
-  orderBy,
-  serverTimestamp,
-} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBou4EJ0VoD0AUInqWC_LvJ9uxojjJBZ_c",
-  authDomain: "shreejienterprise-e4ce5.firebaseapp.com",
-  projectId: "shreejienterprise-e4ce5",
-  storageBucket: "shreejienterprise-e4ce5.firebasestorage.app",
-  messagingSenderId: "693628548007",
-  appId: "1:693628548007:web:f195851ad553e462e53c82",
-  measurementId: "G-0109HCYPVK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase ONCE
 const app = initializeApp(firebaseConfig);
 
-// Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Export Firebase functions
-export {
-  signInWithEmailAndPassword,
-  signOut,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-  query,
-  orderBy,
-  serverTimestamp,
-};
