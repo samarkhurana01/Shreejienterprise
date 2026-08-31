aad1f8ed47f0ddb576fe1378b7948fc49f906b77
samarkhurana01 <samarkhurana014@gmail.com>
2026-08-31 10:39:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 
@@ -8,8 +7,6 @@ ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
